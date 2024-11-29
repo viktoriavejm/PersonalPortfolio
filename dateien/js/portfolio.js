f3a05e7365947461497219f73228ad99e131032e
Vejmelek Viktoria <viktoria.vejmelek@gmx.at>
2024-11-29 12:42:15 +0100

    projekt updated

--- dateien/js/portfolio.js
+++ dateien/js/portfolio.js
@@ -30,7 +30,7 @@ const beschreibungen = [
   ];
   
 
-function loadProjects(id) {
+function loadFotoG() {
 
     document.getElementById('project_back').style.display = 'grid';
 
@@ -39,8 +39,8 @@ function loadProjects(id) {
     let stringValue = ""
     let backValue = `<p id="back" onclick="projectsBack()">Back</p>`
     //stringValue += `<p id="back" onclick="projectsBack()">Back</p>`
-    switch(id){
-        case 1:
+    
+            stringValue = ""
             for (let i = 0; i < beschreibungen.length; i++) {
                stringValue += `
 
@@ -56,35 +56,132 @@ function loadProjects(id) {
             document.getElementById("projects_main").style.display = 'none';
             document.getElementById("projects_main_headline").style.display = 'none';
 
-        case 2:
-
-        for (let i = 0; i < projectDescriptions.length; i++) {
-            stringValue += `
-
-             <div class="projekte_web">
-                <div class="projekt_img">
-                    <img class="project_main_img_photos" src="../../fotos/projekte/webseiten/pr${i}.png" alt="projekt${i}">
-                </div>
-                 
-                <div class="projekt_text">
-                    <p class="foto_beschreibing">${projectDescriptions[i]}</p>
-                </div>
-                 
-             </div>`
-         }
-
+       
          console.log(stringValue)
          document.getElementById('project_back').innerHTML = backValue;
          document.getElementById('project_info_content').innerHTML = stringValue;
          document.getElementById("projects_main").style.display = 'none';
          document.getElementById("projects_main_headline").style.display = 'none';
+}
 
-        case 3:
-            break
+function loadWeb() {
 
-        case 4:
-            break
-    }
+    document.getElementById('project_back').style.display = 'grid';
+
+    document.getElementById('project_info_content').style.display = 'grid';
+    
+    let stringValue = ""
+    let backValue = `<p id="back" onclick="projectsBack()">Back</p>`
+
+    stringValue = ""
+    for (let i = 0; i < projectDescriptions.length; i++) {
+        stringValue += `
+
+         <div class="projekte_web">
+            <div class="projekt_img">
+                <img class="project_main_img_photos" src="../../fotos/projekte/webseiten/pr${i}.png" alt="projekt${i}">
+            </div>
+             
+            <div class="projekt_text">
+                <p class="foto_beschreibing">${projectDescriptions[i]}</p>
+            </div>
+             
+         </div>`
+     }
+
+     console.log(stringValue)
+     document.getElementById('project_back').innerHTML = backValue;
+     document.getElementById('project_info_content').innerHTML = stringValue;
+     document.getElementById("projects_main").style.display = 'none';
+     document.getElementById("projects_main_headline").style.display = 'none';
+
+
+  console.log(stringValue)
+  document.getElementById('project_back').innerHTML = backValue;
+  document.getElementById('project_info_content').innerHTML = stringValue;
+  document.getElementById("projects_main").style.display = 'none';
+  document.getElementById("projects_main_headline").style.display = 'none';
+
+}
+
+function loadProgramming() {
+
+    document.getElementById('project_back').style.display = 'grid';
+
+    document.getElementById('project_info_content').style.display = 'grid';
+    
+    let stringValue = ""
+    let backValue = `<p id="back" onclick="projectsBack()">Back</p>`
+
+    stringValue = ""
+    for (let i = 0; i < projectDescriptions.length; i++) {
+        stringValue += `
+
+         <div class="projekte_web">
+            <div class="projekt_img">
+                <img class="project_main_img_photos" src="../../fotos/projekte/webseiten/pr${i}.png" alt="projekt${i}">
+            </div>
+             
+            <div class="projekt_text">
+                <p class="foto_beschreibing">${projectDescriptions[i]}</p>
+            </div>
+             
+         </div>`
+     }
+
+     console.log(stringValue)
+     document.getElementById('project_back').innerHTML = backValue;
+     document.getElementById('project_info_content').innerHTML = stringValue;
+     document.getElementById("projects_main").style.display = 'none';
+     document.getElementById("projects_main_headline").style.display = 'none';
+
+
+  console.log(stringValue)
+  document.getElementById('project_back').innerHTML = backValue;
+  document.getElementById('project_info_content').innerHTML = stringValue;
+  document.getElementById("projects_main").style.display = 'none';
+  document.getElementById("projects_main_headline").style.display = 'none';
+
+}
+
+function loadElem() {
+
+    document.getElementById('project_back').style.display = 'grid';
+
+    document.getElementById('project_info_content').style.display = 'grid';
+    
+    let stringValue = ""
+    let backValue = `<p id="back" onclick="projectsBack()">Back</p>`
+
+    stringValue = ""
+    for (let i = 0; i < projectDescriptions.length; i++) {
+        stringValue += `
+
+         <div class="projekte_web">
+            <div class="projekt_img">
+                <img class="project_main_img_photos" src="../../fotos/projekte/webseiten/pr${i}.png" alt="projekt${i}">
+            </div>
+             
+            <div class="projekt_text">
+                <p class="foto_beschreibing">${projectDescriptions[i]}</p>
+            </div>
+             
+         </div>`
+     }
+
+     console.log(stringValue)
+     document.getElementById('project_back').innerHTML = backValue;
+     document.getElementById('project_info_content').innerHTML = stringValue;
+     document.getElementById("projects_main").style.display = 'none';
+     document.getElementById("projects_main_headline").style.display = 'none';
+
+
+  console.log(stringValue)
+  document.getElementById('project_back').innerHTML = backValue;
+  document.getElementById('project_info_content').innerHTML = stringValue;
+  document.getElementById("projects_main").style.display = 'none';
+  document.getElementById("projects_main_headline").style.display = 'none';
+
 }
 
 function projectsBack() {
